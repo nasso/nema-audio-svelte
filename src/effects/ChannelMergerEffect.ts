c47a5370363ab7234fc2db30ab5b71b0a39e751e
nasso <nassomails@gmail.com>
2021-10-13 17:17:01 +0200

    Add parameters to channel merger and output

--- src/effects/ChannelMergerEffect.ts
+++ src/effects/ChannelMergerEffect.ts
@@ -1,10 +1,43 @@
-import { Effect } from "@api/graph";
+import { Effect, ParameterAccuracy, ParameterType } from "@api/graph";
 
 export default class ChannelMergerEffect extends Effect {
   name = "Channel merger";
   inputs = 2;
 
   constructor() {
-    super([]);
+    super([
+      {
+        name: "Volume A",
+        value: 0.8,
+        min: 0.0,
+        max: 1.0,
+        type: ParameterType.Absolute,
+        accuracy: ParameterAccuracy.Sample,
+      },
+      {
+        name: "Pan A",
+        value: 0.0,
+        min: -1.0,
+        max: 1.0,
+        type: ParameterType.Relative,
+        accuracy: ParameterAccuracy.Sample,
+      },
+      {
+        name: "Volume B",
+        value: 0.8,
+        min: 0.0,
+        max: 1.0,
+        type: ParameterType.Absolute,
+        accuracy: ParameterAccuracy.Sample,
+      },
+      {
+        name: "Pan B",
+        value: 0.0,
+        min: -1.0,
+        max: 1.0,
+        type: ParameterType.Relative,
+        accuracy: ParameterAccuracy.Sample,
+      },
+    ]);
   }
 }
--- src/effects/OutputEffect.ts
+++ src/effects/OutputEffect.ts
@@ -1,10 +1,27 @@
-import { Effect } from "@api/graph";
+import { Effect, ParameterAccuracy, ParameterType } from "@api/graph";
 
 export default class OutputEffect extends Effect {
-  name = "Output (speakers)";
+  name = "Output";
   outputs = 0;
 
   constructor() {
-    super([]);
+    super([
+      {
+        name: "Volume",
+        value: 1.0,
+        min: 0.0,
+        max: 1.2,
+        type: ParameterType.Absolute,
+        accuracy: ParameterAccuracy.Sample,
+      },
+      {
+        name: "Pan",
+        value: 0.0,
+        min: -1.0,
+        max: 1.0,
+        type: ParameterType.Relative,
+        accuracy: ParameterAccuracy.Sample,
+      },
+    ]);
   }
 }
